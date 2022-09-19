357d15f159abce9eb5bd769053f7a32a8a843c79
Jeffrey Chow <16521353@mahasiswa.itb.ac.id>
2022-09-19 14:14:40 +0700

    added gauss method

--- src/codes/methods/Gauss.java
+++ src/codes/methods/Gauss.java
@@ -3,8 +3,18 @@ import codes.ADT.*;
 
 public class Gauss {
     public static Matrix gauss(Matrix m) {
-        Matrix n = new Matrix(2,2);
 
-        return n;
+        for (int j = 0; j < m.cols; j++) {
+            for (int i = j + 1; i < m.rows; i++) {
+                double pem = m.Mtrx[i][j];
+                double pen = m.Mtrx[j][j];
+                for (int k = 0; k < m.cols; k++)
+                {
+                    m.Mtrx[i][k] -= ((pem / pen) * m.Mtrx[j][k]);
+                }
+            }
+        }
+
+        return m;
     }
 }
