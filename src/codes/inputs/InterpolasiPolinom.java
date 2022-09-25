98865f6d45b845168e423cdf247cd9ff48b19fa9
Jimly-Firdaus <16521312@std.stei.itb.ac.id>
2022-09-25 22:36:32 +0700

    Added Interpolasi Polinom base

--- src/codes/inputs/InterpolasiPolinom.java
+++ src/codes/inputs/InterpolasiPolinom.java
@@ -1,7 +1,15 @@
 package codes.inputs;
 
+import codes.ADT.*;
+import codes.ADT.constructors.*;
+import codes.ADT.primitives.*;
+import codes.inputs.*;
+import codes.methods.*;
+
 public class InterpolasiPolinom {
-    public static void main(String[] args) {
-        
+    public static double polynom() {
+        double result = 0.0;
+
+        return result;
     }
 }
