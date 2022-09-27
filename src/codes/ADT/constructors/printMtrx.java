ed42f26d231a1f1d5c6c9be9c60598ad8b57a736
Jeffrey Chow <16521353@mahasiswa.itb.ac.id>
2022-09-27 09:20:29 +0700

    adding printMtrx

--- src/codes/ADT/constructors/printMtrx.java
+++ src/codes/ADT/constructors/printMtrx.java
@@ -1,5 +1,32 @@
 package codes.ADT.constructors;
 
+import java.util.Scanner;
+import java.io.IOException;
+import codes.ADT.Matrix;
+import codes.methods.submethods.menuCheck;
+
 public class printMtrx {
-    
+    public static Scanner scanner = new Scanner(System.in);
+    public static void main(Matrix matrix) {
+        System.out.println("Output Types:");
+
+        String[] methods = {
+                "Console",
+                "File txt"
+        };
+
+        int inputType = menuCheck.main(1, 2, methods);
+
+        if (inputType == 1){
+            printMtrxConsole.printMatrix(matrix);
+        } else {
+            System.out.println("Output file name: ");
+            String pathname = scanner.nextLine();
+            try {
+                printMtrxFile.printMatrix(matrix, pathname);
+            } catch (IOException ex) {
+                System.out.println("No such files.");
+            }
+        }
+    }
 }
--- src/codes/inputs/inputSPL.java
+++ src/codes/inputs/inputSPL.java
@@ -78,6 +78,9 @@ public class inputSPL {
             System.out.println("SPL memiliki solusi unik");
 
             double[] unik = uniqueCase(m);
+        
+            // printMtrx.main(m);
+
             try {
                 // Need to create input filename for pathname
                 printMtrxFile.printMatrix(unik, "output_1.txt");
