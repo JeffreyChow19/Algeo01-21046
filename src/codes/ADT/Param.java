508ee2c5bdedb413ea00e347945aee9fc67ffa51
Jeffrey Chow <16521353@mahasiswa.itb.ac.id>
2022-09-28 12:49:30 +0700

    merging

---
+++ src/codes/ADT/Param.java
@@ -0,0 +1,11 @@
+package codes.ADT;
+
+public class Param {
+    public double angka;
+    public char abjad;
+
+    public Param() {
+        this.angka = -9999.99;
+        this.abjad = '*';
+    }
+}
--- src/codes/tes.java
+++ src/codes/tes.java
@@ -6,12 +6,16 @@ import codes.inputs.inputSPL;
 import codes.methods.Gauss;
 import codes.methods.GaussJordan;
 import codes.ADT.Matrix;
+import codes.ADT.Param;
 
 public class tes {
     public static void main(String[] args) {
-        Matrix m = createMtrx.main(false);
-        m = Gauss.gauss(m);
-        printMtrx.main(m);
+        Param x = new Param();
+        x.abjad = 'a';
+        x.angka = 3;
+
+        System.out.println(x.abjad);
+        System.out.println(x.angka);
 
     }
 }
